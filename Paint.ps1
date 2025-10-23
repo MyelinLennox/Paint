87a767f1b4f5ba8ce561a126f80680b60286114b
Myelin Lennox <118149338+MyelinLennox@users.noreply.github.com>
2025-10-22 22:27:29 -0500

    Create Paint.ps1

---
+++ Paint.ps1
@@ -0,0 +1,15 @@
+﻿Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+
+
+
+
+
+$formWindow = New-Object System.Windows.Forms.Form
+
+$formWindow.Text = "Paint!"
+$formWindow.Width = 1200
+$formWindow.Height = 800
+
+$formWindow.ShowDialog()
